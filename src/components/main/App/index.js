960af822e61a8e0d460569c013f1f50b072e2e36
MariiaPunda <door729@gmail.com>
2023-02-16 19:47:29 +0100

    Add react-leaflet to display a map

--- src/components/main/App/index.js
+++ src/components/main/App/index.js
@@ -1,4 +1,5 @@
 import { useFela } from 'react-fela'
+import { MapContainer, TileLayer } from 'react-leaflet'
 import styles from './styles'
 import Logo from './Logo'
 import Action from '../../shared/Action'
@@ -20,6 +21,19 @@ const App = () => {
       </header>
       <main role='main' id='main'>
         <VisuallyHidden as='h1'>Find a home for rent</VisuallyHidden>
+        <div id='map'>
+          <MapContainer
+            center={[51.505, -0.09]}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{ width: '100%', height: '100vh' }}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+            />
+          </MapContainer>
+        </div>
       </main>
     </>
   )
