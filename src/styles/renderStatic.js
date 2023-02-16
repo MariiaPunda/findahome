d9fccbc6b00d906b3eb83f04fea7cbf6ca8772f3
MariiaPunda <door729@gmail.com>
2023-02-16 19:12:56 +0100

    Add static styles and variables

--- src/styles/renderStatic.js
+++ src/styles/renderStatic.js
@@ -25,4 +25,26 @@ export default renderer => {
     },
     'body'
   )
+
+  renderer.renderStatic(
+    {
+      '--background-main': '#FFFFFF',
+      '--background-secondary': '#F5F5F5',
+      '--background-action': '##E37C27',
+      '--typography-main': '#000000',
+      '--typography-secondary': '#D3D3D3',
+      '--typography-action': '#FFFFFF',
+      '--spacing-xs': '0.25rem',
+      '--spacing-s': '0.5rem',
+      '--spacing-m': '1rem',
+      '--spacing-l': '1.25rem',
+      '--spacing-xl': '1.5rem',
+      '--font-size-xs': '0.8rem',
+      '--font-size-s': '1rem',
+      '--font-size-m': '1.25rem',
+      '--font-size-l': '1.5rem',
+      '--font-size-xl': '1.75rem',
+    },
+    ':root'
+  )
 }
---
+++ src/styles/variables.js
@@ -0,0 +1,2 @@
+export const TEXT_SIZES = ['xs', 's', 'm', 'l', 'xl']
+export const TEXT_COLORS = ['main', 'secondary', 'action']
